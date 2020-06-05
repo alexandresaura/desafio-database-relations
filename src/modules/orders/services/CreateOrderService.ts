0e1cdb458a9e3d5318fb1a8bffc07a326ec9a61a
Alexandre Saura <alexandresaura21@gmail.com>
2020-06-05 14:04:20 -0300

    Desafio concluído

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,57 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+
+    const productsIds = products.map(product => ({ id: product.id }));
+
+    const productsDB = await this.productsRepository.findAllById(productsIds);
+
+    const orderProducts = products.map(product => {
+      const productDB = productsDB.find(
+        findProduct => findProduct.id === product.id,
+      );
+
+      if (!productDB) {
+        throw new AppError(`Product with id: ${product.id}, does not exist`);
+      }
+
+      if (productDB.quantity < product.quantity) {
+        throw new AppError(
+          `There is insuficient stock available for the product: ${productDB.name}`,
+        );
+      }
+
+      productDB.quantity -= product.quantity;
+
+      return {
+        product_id: product.id,
+        quantity: product.quantity,
+        price: productDB.price,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(productsDB);
+
+    const productSaved = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return productSaved;
   }
 }
 
